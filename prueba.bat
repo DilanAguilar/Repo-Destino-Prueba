27c6a47163b28b31d702eb9f367769ee28677454
DilanAguilar <dilanaguilarmtz17@gmail.com>
2023-11-07 13:43:46 -0600

    Nuevo archivo

---
+++ prueba.bat
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Repositorio Origen
+git remote set-url --add origin "https://github.com/DilanAguilar/Repositorio-Prueba.git"
+
+git remote set-url --add origin "https://github.com/DilanAguilar/Repo-Destino-Prueba.git"
+
+
+git push
